49e9fb00b98232b77f05b4acbe596acf4178582b
ahmed7g10 <ahmedabdo1084@gmail.com>
2024-10-07 15:27:32 +0300

    17 none strict cookie

--- backend/utils/helpers/genrateTokenAndSetCookie.js
+++ backend/utils/helpers/genrateTokenAndSetCookie.js
@@ -7,7 +7,7 @@ const genrateTokenAndSetCookie = (userId, res) => {
         httpOnly: false,// xss protaction
         maxAge: 15 * 24 * 60 * 60 * 1000,
         // maxAge: 1 * 60 * 1000, // 1 minute
-        sameSite: 'strict', //csrf attacks protactions
+        sameSite: 'None', //csrf attacks protactions
         path: '/'
     })
 
